f394fc09b60ba9849415cbd6df9c98fee66cc99e
1shivamgit <shvmshrm9889@gmail.com>
2025-05-06 16:15:58 +0530

    Update viewdustbin.php

--- Backend/viewdustbin.php
+++ Backend/viewdustbin.php
@@ -46,3 +46,32 @@
             </div>
         </div>
     </header>
+  
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg bg-light shadow-lg">
+        <div class="container">
+            <a class="navbar-brand" href="/Bin-Buddy/index.html">
+                <img src="/Bin-Buddy/images/logo.webp" class="logo img-fluid" alt="">
+                <span>
+                    Bin Buddy
+                    <small>Building a Green Future</small>
+                </span>
+            </a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item ms-3">
+                        <a class="nav-link custom-btn custom-border-btn btn" href="#"><b>Volunteer's Application</b></a>
+                    </li>
+                    <li class="nav-item ms-3">
+                        <a class="nav-link custom-btn custom-border-btn btn" href="/Bin-Buddy/finished-tasks.html"><b>Dustbin's Data</b></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
