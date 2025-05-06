e0157b33455c4ef94fb01671b46dc368d14eebce
1shivamgit <shvmshrm9889@gmail.com>
2025-05-06 14:25:25 +0530

    Create dustbinqw.php

---
+++ Backend/dustbinqw.php
@@ -0,0 +1,21 @@
+<?php
+// dustbinqw.php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" href="/Bin-Buddy/images/logo.webp" sizes="16x16" type="image/x-icon">
+  <link rel="icon" href="/Bin-Buddy/favicon-32x32.png" sizes="32x32" type="image/png">
+  <link rel="icon" href="/Bin-Buddy/favicon-48x48.png" sizes="48x48" type="image/png">
+  <link rel="icon" href="/Bin-Buddy/favicon-96x96.png" sizes="96x96" type="image/png">
+  
+  <!-- CSS FILES -->
+  <link href="/Bin-Buddy/css/bootstrap.min.css" rel="stylesheet">
+  <link href="/Bin-Buddy/css/bootstrap-icons.css" rel="stylesheet">
+  <link href="/Bin-Buddy/css/binbuddy.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css">
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <title>Bin Buddy - Admin Panel</title>
+</head>
