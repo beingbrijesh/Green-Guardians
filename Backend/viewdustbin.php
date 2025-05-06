1c4772a0f9c7c0934888d5ba0d7ef4db2b4512e3
1shivamgit <shvmshrm9889@gmail.com>
2025-05-06 16:15:34 +0530

    Update viewdustbin.php

--- Backend/viewdustbin.php
+++ Backend/viewdustbin.php
@@ -19,3 +19,30 @@
   
   <title>View Registered Dustbins</title>
 </head>
+<body>
+    <!-- Header -->
+    <header class="site-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-12 d-flex flex-wrap">
+                    <p class="d-flex me-4 mb-0">
+                        <i class="bi-geo-alt me-2"></i>
+                        Ghaziabad, Uttar Pradesh, India
+                    </p>
+                    <p class="d-flex mb-0">
+                        <i class="bi-envelope me-2"></i>
+                        <a href="mailto:info@company.com">info@binbuddy.org</a>
+                    </p>
+                </div>
+                <div class="col-lg-3 col-12 ms-auto d-lg-block d-none">
+                    <ul class="social-icon">
+                        <li class="social-icon-item"><a href="https://twitter.com/" class="social-icon-link bi-twitter"></a></li>
+                        <li class="social-icon-item"><a href="https://facebook.com/" class="social-icon-link bi-facebook"></a></li>
+                        <li class="social-icon-item"><a href="https://instagram.com/" class="social-icon-link bi-instagram"></a></li>
+                        <li class="social-icon-item"><a href="https://youtube.com/" class="social-icon-link bi-youtube"></a></li>
+                        <li class="social-icon-item"><a href="https://web.whatsapp.com/" class="social-icon-link bi-whatsapp"></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header>
